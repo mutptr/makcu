80f4d88e35aade4c7302827c8c524fb9dc8101e7
mutptr <bronzer.megaton.0v@icloud.com>
2025-07-14 11:07:20 +0900

    bincode decode limit 추가

--- makcu-bridge-server/src/protocol.rs
+++ makcu-bridge-server/src/protocol.rs
@@ -6,7 +6,7 @@ macro_rules! impl_decode {
     ($ty:ty) => {
         impl Decode for $ty {
             fn from_slice(data: &[u8]) -> anyhow::Result<Self> {
-                let config = bincode::config::legacy();
+                let config = bincode::config::legacy().with_limit::<1024>();
                 let (decoded, _) = bincode::decode_from_slice::<Self, _>(data, config)?;
                 Ok(decoded)
             }
